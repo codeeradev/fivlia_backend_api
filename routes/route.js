4a6d01e0f7163c6b39e648208dc4a1d523a946aa
Your Name <you@example.com>
2026-04-06 12:20:37 +0530

    get banner update and other updates for admin for adding cat brand and other things

--- routes/route.js
+++ routes/route.js
@@ -3,6 +3,7 @@ const upload = require("../midllerware/multer");
 const uploadCsv = require("../midllerware/csvUpload.js");
 const router = express.Router();
 const verifyToken = require("../midllerware/authToken");
+const typeCategoryResolver = require("../midllerware/typeCategoryResolver.js");
 const attachStaffRole = require("../midllerware/attachStaffRole.js");
 const checkPermission = require("../midllerware/checkPermission.js");
 
