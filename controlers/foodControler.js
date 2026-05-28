5607f0026ebdfd1801772d5a55665ec842d392eb
Your Name <you@example.com>
2026-05-28 11:08:50 +0530

    .

--- controlers/foodControler.js
+++ controlers/foodControler.js
@@ -1,7 +1,7 @@
 const foodTypeModel = require("../modals/foodType");
 const Seller = require("../modals/store");
 const Rating = require("../modals/rating");
-const Stock = require("../models/stockModel");
+const Stock = require("../modals/StoreStock");
 
 exports.addFood = async (req, res) => {
   try {
